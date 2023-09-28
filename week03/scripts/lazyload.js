b5a65c83ef7e30202ff2a38879289d878fc2a59d
keelw <wjordankeel@gmail.com>
2023-09-27 18:44:03 -0600

    lazy loading test

---
+++ week03/scripts/lazyload.js
@@ -0,0 +1,7 @@
+let lastModifiedString = new Date(document.lastModified);
+oLastModif = lastModifiedString.toUTCString();
+document.getElementById('lastModified').innerHTML = `Last modified: ${oLastModif}`;
+
+let d = new Date();
+let currentYear = d.getFullYear();
+document.getElementById('copyRight').innerHTML = `&copy ${currentYear} William Keel &#127482;&#127480`;
