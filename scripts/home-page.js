69dcb3ef411c76421a0f0812ef7fa2bc9bb07d90
keelw <wjordankeel@gmail.com>
2023-09-28 19:36:08 -0600

    week03 team activity start

--- scripts/home-page.js
+++ scripts/home-page.js
@@ -1,10 +1,30 @@
 const hamButton = document.querySelector('#menu');
 const navigation = document.querySelector('.navigation');
+const visitsDisplay = document.querySelector(".visits")
 
 hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
 	hamButton.classList.toggle('open');
 });
+
+// Get the stored value of the visits key in local storage
+// if it does not exist, create it and assign 0.
+let numVisits = Number(window.localStorage.getItem("numBisits-ls")) || 0;
+
+// Determine if this is the first visit or display the number of visits
+if (numVisits !== 0) {
+    visitsDisplay.textContent = numVisits;
+}
+
+else {
+    visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`;
+}
+
+// Make sure to increment the number by 1
+numVisits++;
+
+// Store the new visit total into localSotrage
+localStorage.setItem("numVisits-ls", numVisits);
 
 const modeButton = document.querySelector("#mode");
 const main = document.querySelector("main");
