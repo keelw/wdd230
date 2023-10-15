13c7505a15ea00037f3b9009573786a1e3f8ce48
keelw <wjordankeel@gmail.com>
2023-10-14 22:29:32 -0600

    get JSON file in js script

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,7 @@
+// variables
+const url = "https://keelw.github.io/wdd230/chamber/data/members.json";
+const cards = document.querySelector("#directory");
+
+// get the data from the JSON file
+
+// display the data
